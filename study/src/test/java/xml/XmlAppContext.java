584e7223a4afa838e781aa7e53e67bc22eaef73c
USER <USER@DESKTOP-2MVNI9T>
2021-07-11 19:20:26 +0900

    GenericXmlApplicationContext

---
+++ study/src/test/java/xml/XmlAppContext.java
@@ -0,0 +1,21 @@
+package xml;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import member.MemberService;
+
+public class XmlAppContext {
+	
+	@Test
+	 void xmlAppContext() {
+	 ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+	 
+	 MemberService memberService = ac.getBean("memberService", MemberService.class);
+	 Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+	 }
+
+}
