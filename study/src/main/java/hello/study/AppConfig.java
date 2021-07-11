17120698510d0d9084be3cf380381fe32cdc2f5c
USER <USER@DESKTOP-2MVNI9T>
2021-07-11 13:30:18 +0900

    2021.07.11 AOP 중복 리팩터링 해결

--- study/src/main/java/hello/study/AppConfig.java
+++ study/src/main/java/hello/study/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.study;
 
+import discount.DiscountPolicy;
 import discount.FixDiscountPolicy;
+import member.MemberRepository;
 import member.MemberService;
 import member.MemberServiceImpl;
 import member.MemoryMemberRepository;
@@ -9,12 +11,20 @@ import order.OrderServiceImpl;
 
 public class AppConfig {
 	
+	private MemberRepository memberRepository() { // AOP 중복 리팩터링
+		return new MemoryMemberRepository();
+	}
+	
 	public MemberService memberService() {
-		return new MemberServiceImpl(new MemoryMemberRepository());
+		return new MemberServiceImpl(memberRepository());
 	}
 	
 	public OrderService orderService() {
-		return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+		return new OrderServiceImpl(memberRepository(), discountPolicy());
+	}
+	
+	public DiscountPolicy discountPolicy() {
+		return new FixDiscountPolicy();
 	}
 	
 }
