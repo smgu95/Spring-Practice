b7de70f78548e1af679bc7eb9cf45b4cfa91459f
USER <USER@DESKTOP-2MVNI9T>
2021-08-08 21:45:02 +0900

    ?

--- study/src/test/java/xml/XmlAppContext.java
+++ study/src/test/java/xml/XmlAppContext.java
@@ -1,21 +1,24 @@
 package xml;
 
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
-import member.MemberService;
+import hello.study.member.MemberService;
 
 public class XmlAppContext {
-	
+
 	@Test
 	 void xmlAppContext() {
 	 ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
 	 
 	 MemberService memberService = ac.getBean("memberService", MemberService.class);
-	 Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+	 assertThat(memberService).isInstanceOf(MemberService.class);
 	 }
+	
 
 }
